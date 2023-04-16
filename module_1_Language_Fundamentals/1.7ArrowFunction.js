571a12e12a0ceca17f99f6ae4c83dc33dfe6470c
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-04-16 17:58:38 +0530

    Arrow Functions

--- module_1_Language_Fundamentals/1.7ArrowFunction.js
+++ module_1_Language_Fundamentals/1.7ArrowFunction.js
@@ -35,4 +35,42 @@ let fun3 = function(){
 }
 fun3();
 
+//summary
+function fun4(){
+    //code
+}
 
+fun5 = function(){
+    // code
+}
+
+let fun6 = () => {
+    // code
+}
+
+let sum = function(a){
+    console.log(a);
+}
+sum(5);
+
+let sum1 = (a) => {
+    console.log(a);
+}
+sum(5);
+
+//can also be written as
+let sum2 = a => {
+    console.log(a);
+}
+sum(5);
+
+sum(5, 6);
+
+let sum3 = (a, b) => {
+    return a + b;
+}
+console.log(sum3(5, 6));
+
+//when arrow function just one statement and that too return statement - no need of curly braces
+let sum4 = (a, b) => a + b;
+console.log(sum4(4, 5));
