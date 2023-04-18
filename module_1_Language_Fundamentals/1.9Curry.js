30e33ca891174e1168eb3427823fa6b8c5e1d47c
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-04-18 23:52:38 +0530

    curry concept and code

---
+++ module_1_Language_Fundamentals/1.9Curry.js
@@ -0,0 +1,40 @@
+// 1.
+const sum = function (a){
+    return function (b){
+        return a + b;
+    }
+}
+const cl = sum(5);
+const ans = cl(6);
+console.log(ans);
+
+// 2.
+const ans1 = sum(5)(6);
+console.log(ans1);
+
+// 3. 
+const sum1 = (a) => {
+    return (b) => {
+        return a + b;
+    }
+}
+const ans2 = sum1(5)(6);
+console.log(ans2);
+
+// 4.
+const sum2 = a =>  b => a + b;
+
+const ans3 = sum2(5)(6);
+console.log(ans3);
+
+//5. 
+const PriceCalc = (price) => {
+    return (dPer) => {
+        return price*dPer;
+    }
+}
+
+const discountAmount = PriceCalc(300);
+console.log(discountAmount(0.5));
+console.log(discountAmount(0.3));
+console.log(discountAmount(0.2));
