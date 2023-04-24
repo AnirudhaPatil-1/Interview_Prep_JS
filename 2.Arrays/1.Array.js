36f80cd5135239544c01b5d8138f18ea748c27b7
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-04-24 17:45:32 +0530

    1. Array traversal

--- 2.Arrays/1.Array.js
+++ 2.Arrays/1.Array.js
@@ -3,13 +3,13 @@ let arr = [];
 let arr1 = [4, 5, 6];
 let arr2 = ["hello", 10, true];
 
-// console.log(arr);
-// console.log(arr1);
-// console.log(arr2);
+console.log(arr);
+console.log(arr1);
+console.log(arr2);
 
-// console.log(arr2.length);
-// console.log(arr2[0]);
-// console.log(arr2[arr2.length - 1]);
+console.log(arr2.length);
+console.log(arr2[0]);
+console.log(arr2[arr2.length - 1]);
 
 //2. array  having elements as object literal
 console.log("------------- 2. array  having elements as object literal ------------------");
@@ -19,9 +19,9 @@ let arrObj = [
     {pcode: 3, pName: "Orange"}
 ]
 
-// console.log(arrObj);
-// console.log(arrObj[0]); //{ pcode: 1, pName: 'Apple' }
-// console.log(arrObj[0].pName); //Apple
+console.log(arrObj);
+console.log(arrObj[0]); //{ pcode: 1, pName: 'Apple' }
+console.log(arrObj[0].pName); //Apple
 
 //3. array traversal
 console.log("------------- 3  array traversal - for in - for of ------------------");
@@ -30,7 +30,7 @@ let arr4 = ["ES5", "ES6", "ES7", "ES8"];
 for(let i = 0; i < arr4.length; i++){
     console.log(arr4[i]);
 }
-// console.log(arr4);
+console.log(arr4);
 
 //for in - gives index
 for(let i in arr4){
@@ -48,8 +48,8 @@ for(let i of arr4){
 })
 */
 console.log("----------- 4. forEach -------------");
-arr4.forEach(function(value, index, arr, add){
-    // console.log(value, index, arr);
+arr4.forEach(function(value, index, arr, address){
+    console.log(value, index, arr);
 
 
 })
