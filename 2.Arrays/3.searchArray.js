dd03662a5b648d528a42508fca5561f847478903
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-04-25 19:25:21 +0530

    search methods in array - indexOf(), lastIndexOf(), includes(), find(), findIndex(), filter()

--- 2.Arrays/3.searchArray.js
+++ 2.Arrays/3.searchArray.js
@@ -39,4 +39,39 @@ console.log("----------- 3. includes() ------- ");
 
 console.log(arr1.includes("one")); //true
 
+//find(), findIndex(), filter
 
+//4.find
+console.log("----------- 4.find ------- ");
+
+// returns value
+//else returns undefined
+//has similar syntax as forEach()
+let arr10 = [1000, 2000, 3000, 4000, 5000];
+
+const rVal = arr10.find((value, index, arr) => {
+    return value > 3000;
+});
+console.log(rVal); //4000
+
+//5.findIndex()
+console.log("----------- 5.findIndex() ------- ");
+
+// returns index
+//else returns undefined
+const rIndex = arr10.findIndex((value, index, arr) => {
+    return value > 3000;
+})
+console.log(rIndex); //3
+
+//6. filter()
+console.log("----------- 6. filter() ------- ");
+
+// returns a new array with elements which return the condition true
+// if no element meets condition returns empty array 
+
+const rValue = arr10.filter((element, index, arr, thisArr) => {
+    //dont write just "this", it will give error
+    return element > 3000;
+});
+console.log(rValue); //[ 4000, 5000 ]
