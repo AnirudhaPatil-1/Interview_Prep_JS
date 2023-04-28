a9311cabbf55316f3e524ff592ca4b6f7a64b346
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-04-28 22:33:22 +0530

    mock google meet

---
+++ 3.String/mock.js
@@ -0,0 +1,74 @@
+// 23. string basics	-         string basics, escape sequence, special characters, emoticon
+// 24. ES6 template literal	 -    ``, template literal format, advantages
+// 25. .length, search methods-   indexOf, lastIndexOf() & search()
+// 26. extraction methods	   -  substring extraction methods - slice(), substr(), substring & char extraction methods- chatAt(), charCodeAt
+// 27. case conversion, replace()-case conversion - toLowerCase(), toUpperCase() & replace()
+
+// String basics - 
+// '', "", ``,
+
+let str = "hello  u00A9 world";
+console.log(str);
+str = "\u00A9";
+console.log(str);  //©
+str = "\{}";
+
+//ES6 template literal
+// ``
+let name = "Gaurav";
+console.log(`my name is ${name}`);
+str = `my name
+            is 
+                Gaurav`;
+// console.log(str);
+
+//.length & search methods - indexOf(), lastIndexOf() and search
+
+str = "hello world hello world";
+console.log(str.indexOf("hello")); //0
+console.log(str.indexOf("hello", 10)); //12
+
+console.log(str.lastIndexOf("hello")); //12
+console.log(str.lastIndexOf("hello")); //12
+
+console.log(str[3]);
+
+console.log(str.search("hello")); //0
+
+console.log(str.length); //23
+
+
+//extraction methods
+console.log("-----extraction methods------");
+//SUBSTRING - slice(), substr(), substring
+//char - charAt(), charCodeAt()
+
+//substring extraction methods
+// 1.slice
+console.log("-1.slice-");
+str = "hello world";
+console.log(str.slice(1, 3));
+console.log(str.slice(1));
+console.log(str.slice(-5, -1)); //worl
+
+//2. substr
+console.log(str.substr(1, 3)); //ell
+console.log(str.substr(-5, 2)); //wo
+
+//3. substring
+console.log("---3. substring--------");
+console.log(str.substring(1, 3)); 
+console.log(str.substring(-3)); //hello world
+
+
+//extracting char methods
+// 1. charAt()
+console.log(str.charAt(0)); //h
+console.log(str.charCodeAt(0)); //104
+
+//case conversion
+str = "Gaurav";
+console.log(str.toUpperCase()); //GAURAV
+console.log(str.toLowerCase()); //gaurav
+
+
