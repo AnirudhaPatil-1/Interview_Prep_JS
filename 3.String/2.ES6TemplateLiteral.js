24836af5ab4eda4583fd104543246cd1ece0f4d0
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-04-28 16:24:37 +0530

    template literal format, advantages

---
+++ 3.String/2.ES6TemplateLiteral.js
@@ -0,0 +1,18 @@
+//template literal
+// 1. variable in string
+let name = "Anirudha"
+let str = `Hello my name is ${name}`;
+console.log(str);
+
+// 2. store the given format
+str = `this stores
+            the format  
+                as it is saved inside 
+                        backticks`;
+console.log(str);
+/*output -> 
+this stores
+            the format  
+                as it is saved inside 
+                        backticks
+*/                        
