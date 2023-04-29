04ca6552fa63a327f3dfbdd809f37d1ad1ce62ca
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-04-29 23:51:15 +0530

    date methods

---
+++ 4.date&Time.js/2.dateMethods.js
@@ -0,0 +1,13 @@
+//new Date() is both used for date and time
+
+let dt = new Date();
+console.log(dt); //2023-04-29T18:18:58.297Z
+
+// 1. getFullYear()
+console.log(dt.getFullYear()); //2023
+
+//2. getMonth()
+console.log(dt.getMonth()); //3 -> INDEX so the month is April
+
+
+
