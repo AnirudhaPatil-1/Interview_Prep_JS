82cda65464d6af411995bccd96eb4af5cc25d760
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-04-30 18:09:27 +0530

    this, this in arrow function

---
+++ 5.OOJS/2.this.js
@@ -0,0 +1,18 @@
+console.log(this); //{}
+
+
+function test(){
+    console.log(this);
+}
+// test();
+
+let obj = {
+    test (){
+        console.log(this);
+    },
+    test1: () =>{
+        console.log(this);
+    }
+}
+obj.test(); //{ test: [Function: test] }
+obj.test1(); //{} 
