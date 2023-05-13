5b6c619016f323446bbc018e83ef87db82f72ab1
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-05-13 21:14:27 +0530

    resolve, reject -> .then, .catch, .finally

--- 9.Asynch/2.Promise.js
+++ 9.Asynch/2.Promise.js
@@ -1,6 +1,21 @@
 let pr = new Promise(( resolve, reject) =>{
+    //statement
+    // resolve();
+    // resolve("Data Processed");
+    reject("data not processed");
     
-    resolve();
+
 })
 
-console.log(pr); //Promise { undefined }
+// console.log(pr); //Promise { undefined }
+// console.log(pr); //Promise { 'Data Processed' }
+// console.log(pr); //Promise { <rejected> 'data not processed' }  -> also throws error
+
+pr.then((msg) => {
+    console.log(msg); //Data Processed
+}).catch((err) => {
+    console.log(err); //data not processed
+}).finally(() => { //finally to run either resolve or reject should be meet 
+    console.log("Finishing process"); //Data Processed
+    //Finishing process
+})
