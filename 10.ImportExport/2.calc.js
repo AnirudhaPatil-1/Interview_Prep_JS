dfa698097959c436cc631a7d925f0472da292e96
AnirudhaPatil-1 <aninp2020@gmail.com>
2023-05-19 22:55:53 +0530

    import export - exporting multiple functions and function renaming

---
+++ 10.ImportExport/2.calc.js
@@ -0,0 +1,28 @@
+// 1.
+// export function sum (a, b){
+//     return a + b;
+// }
+// export function cube(a){
+//     return a*a*a;
+// }
+
+// 2.
+// function sum (a, b){
+//     return a + b;
+// }
+// function cube(a){
+//     return a*a*a;
+// }
+
+// export {sum, cube};
+
+//3.
+function sum(a, b){
+    return a + b;
+}
+
+function cube(a){
+    return a*a*a;
+}
+
+export {sum as total, cube as Cube}
